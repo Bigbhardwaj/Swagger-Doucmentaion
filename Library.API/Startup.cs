967b5b0238c7395ffea9880203e1c74455fb4d7f
Bigbhardwaj <bigbhardwaj@gmail.com>
2019-06-21 01:09:54 +0530

    commit the changes

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -96,7 +96,7 @@ namespace Library.API
             }
             else
             {
-                // The default HSTS value is 30 days. 
+                //The default HSTS value is 30 days. 
                 // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
